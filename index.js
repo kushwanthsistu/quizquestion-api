9aad2efd2c7e5b0975ac0966ed0e039ec13e5507
kushwanthsistu <kushwanthsistu@gmail.com>
2022-02-19 20:00:00 +0530

    add server nodejs file

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express') ;
+
+const app = express() ;
+
+app.get('/', (req, res) => {
+    res.send("hello this is from server") ;
+})
+
+
+const port = process.env.PORT || 3000 ;
+
+app.listen(port ,() => {
+    console.log("listening") ;
+})
