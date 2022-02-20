91468816fdd0a1bde1f28924e520f3cfd9f54603
kushwanthsistu <kushwanthsistu@gmail.com>
2022-02-20 08:00:00 +0530

    added the data json file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express') ;
+const gkque = require('./data/generalknowledge.json') ;
 
 const app = express() ;
 
@@ -10,6 +11,12 @@ app.get('/working', (req, res) => {
     res.send("hooray!!! working and we are testing new route") ;
 })
 
+app.get('/generalknowledge', (req, res) => {
+    const l = gkque.length ;
+    let x = Math.random() ;
+    x = Math.floor(x * l) ;
+    res.json(gkque[x]) ;
+})
 
 const port = process.env.PORT || 3000 ;
 
