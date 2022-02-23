20e684e78d4c451fde1534b8a6b0814195900b17
kushwanthsistu <kushwanthsistu@gmail.com>
2022-02-23 18:21:20 +0530

    deleted the cookies because they are not working with XMLHttp requests

--- index.js
+++ index.js
@@ -21,64 +21,24 @@ app.get('/working', (req, res) => {
 app.get('/generalknowledge', (req, res) => {
     const l = gkque.length ;
     let x = findrandom(l) ;
-    if(req.cookies.questionids) {
-        while(req.cookies.questionids.includes(x) && req.cookies.questionids.length < l) {
-            x = findrandom(l) ;
-        }
-        req.cookies.questionids.push(x) ;
-        res.cookie('questionids', req.cookies.questionids) ;
-    }
-    else {
-        res.cookie('questionids', [x]) ;
-    }
     res.json(gkque[x]) ;
 })
 
 app.get('/biology', (req, res) => {
     const l = bioque.length ;
     let x = findrandom(l) ;
-    if(req.cookies.questionids) {
-        while(req.cookies.questionids.includes(x) && req.cookies.questionids.length < l) {
-            x = findrandom(l) ;
-        }
-        req.cookies.questionids.push(x) ;
-        res.cookie('questionids', req.cookies.questionids) ;
-    }
-    else {
-        res.cookie('questionids', [x]) ;
-    }
     res.json(bioque[x]) ;
 })
 
 app.get('/physics', (req, res) => {
     const l = phyque.length ;
     let x = findrandom(l) ;
-    if(req.cookies.questionids) {
-        while(req.cookies.questionids.includes(x) && req.cookies.questionids.length < l) {
-            x = findrandom(l) ;
-        }
-        req.cookies.questionids.push(x) ;
-        res.cookie('questionids', req.cookies.questionids) ;
-    }
-    else {
-        res.cookie('questionids', [x]) ;
-    }
     res.json(phyque[x]) ;
 })
 
 app.get('/chemistry', (req, res) => {
     const l = cheque.length ;
     let x = findrandom(l) ;
-    if(req.cookies.questionids) {
-        while(req.cookies.questionids.includes(x) && req.cookies.questionids.length < l) {
-            x = findrandom(l) ;
-        }
-        req.cookies.questionids.push(x) ;
-        res.cookie('questionids', req.cookies.questionids) ;
-    }
-    else {
-        res.cookie('questionids', [x]) ;
-    }
     res.json(cheque[x]) ;
 })
 
